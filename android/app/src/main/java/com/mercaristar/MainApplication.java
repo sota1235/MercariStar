2f80bfa47388d170ee7b872937f26ad20932cc83
Pramendra Gupta <pramendra@gmail.com>
2017-05-16 18:46:14 +0900

    changes after adding camera module

--- android/app/src/main/java/com/mercaristar/MainApplication.java
+++ android/app/src/main/java/com/mercaristar/MainApplication.java
@@ -3,6 +3,7 @@ package com.mercaristar;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.rnfs.RNFSPackage;
 import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTCameraPackage(),
             new RNFSPackage(),
             new ReactNativeAudioPackage()
       );
