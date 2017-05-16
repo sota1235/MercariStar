c1d8d1118c600accc25094b825c816c5a9ce37ca
Pramendra Gupta <pramendra@gmail.com>
2017-05-16 23:38:06 +0900

    removed png assets

--- android/app/src/main/java/com/mercaristar/MainApplication.java
+++ android/app/src/main/java/com/mercaristar/MainApplication.java
@@ -3,6 +3,7 @@ package com.mercaristar;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.rnfs.RNFSPackage;
 import com.rnim.rn.audio.ReactNativeAudioPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RCTCameraPackage(),
             new RNFSPackage(),
             new ReactNativeAudioPackage()
