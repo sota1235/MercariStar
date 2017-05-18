4849cded2a90a961ca28fdc83f5e3c1aac84f97c
sota1235 <sota1235@gmail.com>
2017-05-18 21:59:59 +0900

    modify: make mock better

--- src/services/search.js
+++ src/services/search.js
@@ -2,32 +2,38 @@
  * @fileoverview Replace with your own API
  */
 export const search = async (keyword) => {
-  const mockResponse = [
-    {
-      title: 'Title',
-      image: 'TODO',
-      price: 1000,
-      description: 'About Title',
+  const mockResponse = {
+    json() {
+      return {
+        data: [
+          {
+            title: 'Title',
+            image: 'TODO',
+            price: 1000,
+            description: 'About Title',
+          },
+          {
+            title: 'Title2',
+            image: 'TODO',
+            price: 5000,
+            description: 'About Title2',
+          },
+          {
+            title: 'Title3',
+            image: 'TODO',
+            price: 1000,
+            description: 'About Title3',
+          },
+          {
+            title: 'Title4',
+            image: 'TODO',
+            price: 5000,
+            description: 'About Title4',
+          },
+        ],
+      };
     },
-    {
-      title: 'Title2',
-      image: 'TODO',
-      price: 5000,
-      description: 'About Title2',
-    },
-    {
-      title: 'Title3',
-      image: 'TODO',
-      price: 1000,
-      description: 'About Title3',
-    },
-    {
-      title: 'Title4',
-      image: 'TODO',
-      price: 5000,
-      description: 'About Title4',
-    },
-  ];
+  };
 
   return mockResponse;
 };
